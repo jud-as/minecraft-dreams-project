de7957a7d431de41717f8966ff8936896f1344f4
jud_as <guilhermevalente.dev@gmail.com>
2026-01-21 11:08:35 -0300

    Adicionando suporte inicial ao mod "Dreams" com configuração do pipeline, itens, blocos e integração com NeoForge. Adicionando textura e item "Will"

--- src/main/java/com/project/dreams/item/ModItems.java
+++ src/main/java/com/project/dreams/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.project.dreams.item;
 
 import com.project.dreams.Dreams;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -9,10 +10,13 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Dreams.MOD_ID);
 
-    public static final DeferredItem<Item> WILL = ITEMS.registerItem(
+    public static final DeferredItem<Item> WILL = ITEMS.registerSimpleItem(
             "will",
-            Item::new,
-            new Item.Properties()
+            p -> p.food(new FoodProperties.Builder()
+                    .alwaysEdible()
+                    .nutrition(1)
+                    .saturationModifier(2f)
+                    .build())
     );
 
     public static void register(IEventBus eventBus) {
