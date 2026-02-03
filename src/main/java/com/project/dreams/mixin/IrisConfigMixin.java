85d3453bb503f33eb34ab5f8c08c59052ac80762
jud_as <guilhermevalente.dev@gmail.com>
2026-02-02 23:00:16 -0300

    Adicionando suporte inicial ao mod "Dreams" com configuração e insercao de Sodium para performance e Iris para carregamento de shaders

---
+++ src/main/java/com/project/dreams/mixin/IrisConfigMixin.java
@@ -0,0 +1,15 @@
+package com.project.dreams.mixin;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(targets = "net.irisshaders.iris.compat.sodium.config.IrisConfig", remap = false)
+public class IrisConfigMixin {
+    @Redirect(method = "registerConfigLate", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;empty()Lnet/minecraft/network/chat/MutableComponent;"))
+    private MutableComponent dreams$provideTooltip() {
+        return Component.translatable("options.iris.shaderPackSelection.sodium_tooltip");
+    }
+}
